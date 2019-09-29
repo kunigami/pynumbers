b904763633affc9ab06b89ac70963250a87d217c
Guilherme Kunigami <kunigami@gmail.com>
2019-09-29 10:15:55 -0700

    return list for prime

--- primes.py
+++ primes.py
@@ -2,7 +2,9 @@ from random import randint
 from exponentiation import modexp
 
 # Return a list of primes less than N
-def sieve (N):
+
+
+def sieve(N):
     """Return a list of primes less than N"""
     isprime = [True]*(N)
 
@@ -13,15 +15,17 @@ def sieve (N):
                 isprime[j] = False
         i += 1
 
-    return filter(lambda x: isprime[x], range(2,N))
+    return list(filter(lambda x: isprime[x], range(2, N)))
 
 # Decides whether a number is prime using the miller-rabin test
+
+
 def miller_rabin(n, k=10):
     if n == 2 or n == 3:
         return True
     if n <= 1 or n % 2 == 0:
         return False
-    for i in range (1, k):
+    for i in range(1, k):
         (s, d) = factor2(n - 1)
         a = randint(2, n-2)
         x = modexp(a, d, n)
@@ -39,6 +43,7 @@ def miller_rabin(n, k=10):
             return False
     return True
 
+
 def factor2(n):
     d = 0
     while n % 2 == 0:
@@ -46,7 +51,6 @@ def factor2(n):
         d += 1
     return (d, n)
 
+
 # implementation selection
 is_prime = miller_rabin
-
-
